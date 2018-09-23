88a18e431f520cc27c1d7f634c8c6bdc6bee9d68
Unknown <pppangpondpp7@gmail.com>
2018-09-24 00:40:29 +0700

    Added loaded posts

--- src/containers/PostList.js
+++ src/containers/PostList.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, Button, AsyncStorage} from 'react-native'
+import { View, Text, Button, AsyncStorage, FlatList } from 'react-native'
 import axios from 'axios';
 import { connect } from 'react-redux'
 import { BASE_URL } from '../constants';
@@ -11,6 +11,13 @@ class PostList extends React.Component {
     title: 'ร้านไหนดี'
   };
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      posts: []
+    }
+  }
+
   componentDidMount() {
     this.loadPosts();
   }
@@ -20,6 +27,13 @@ class PostList extends React.Component {
       <View>
         <Button onPress={() => this.signOut()}
                 title="Sign out" />
+
+        <FlatList
+          data={this.state.posts}
+          renderItem={({item}) => <Text>{item.title}</Text> }
+          keyExtractor={({id}, index) => id.toString()}
+        />
+
       </View>
     )
   }
@@ -31,15 +45,17 @@ class PostList extends React.Component {
 
   loadPosts = () => {
     axios.get(`${BASE_URL}/posts`)
-      .then(function (response) {
-        // handle success
-        // console.log(response);
+      .then((response) => {
+        console.log(response)
+        this.setState({
+          posts: response.data
+        })
       })
-      .catch(function (error) {
+      .catch((error) => {
         // handle error
         console.log(error);
       })
-      .then(function () {
+      .then(() => {
         // always executed
       });
   };
